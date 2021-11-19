a39b42c149e89c0851691c1d36d9ecd4ba573692
Timothy Ponce <timothyjponce@gmail.com>
2021-11-19 14:05:17 -0800

    add contact page

--- src/contact.js
+++ src/contact.js
@@ -4,6 +4,39 @@ export function loadContact () {
 
     const main = document.createElement('div');
     main.id = 'main';
-    content.insertBefore(main, document.querySelector('footer'));
+    content.insertBefore(main, document.querySelector('footer'))
+
+    const iconArray = [
+        ['mail', 'mailto:mail@mail.com', 'img/icons8-mail.svg'],
+        ['instagram', 'https://instagram.com', 'img/icons8-instagram.svg'],
+        ['twitter', 'https://twitter.com', '/img/icons8-twitter.svg'],
+    ]
+
+    const createIcons = (() => {
+        for (let i = 0; i < iconArray.length; i++) {
+        const iconLink = document.createElement('a');
+        iconLink.classList.add('icon');
+        iconLink.id = iconArray[i][0];
+        iconLink.href = iconArray[i][1];
+        main.appendChild(iconLink);
+        const icon = document.createElement('img');
+        icon.src = iconArray[i][2];
+        iconLink.appendChild(icon);
+        }
+    })();
+
+    const createAddress = (() => {
+        const address = document.createElement('div');
+        address.id = 'address';
+        address.innerText = '460 Main St, Venice, CA, 90291';
+        main.appendChild(address);
+    })();
+
+    const createMap = (() => {
+        const map = document.createElement('img');
+        map.id = 'map';
+        map.src = 'img/churro-map.png';
+        main.appendChild(map);
+    })();
 
 };
